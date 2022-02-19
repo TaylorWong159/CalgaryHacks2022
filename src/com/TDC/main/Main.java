561b975335e043035d64b501cb76583037e05c37
dylancon17 <94710755+dylancon17@users.noreply.github.com>
2022-02-19 12:19:26 -0700

    Hi

--- src/com/TDC/main/Main.java
+++ src/com/TDC/main/Main.java
@@ -1,9 +1,18 @@
 package com.TDC.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
 
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
