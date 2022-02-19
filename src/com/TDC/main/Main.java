965a8acc30a74913b7c59404e8455db6305b8055
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2022-02-19 00:17:05 -0700

    added JavaFX

--- src/com/TDC/main/Main.java
+++ src/com/TDC/main/Main.java
@@ -1,9 +1,19 @@
 package com.TDC.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
-
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+		
+	}
+	
+	
 
 }
