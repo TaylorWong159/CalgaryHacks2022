b7af0f1c6bc951cc2fd79856db85aab91c983276
dylancon17 <94710755+dylancon17@users.noreply.github.com>
2022-02-19 17:19:14 -0700

    Skills NAmes

--- src/com/TDC/skills/Skills.java
+++ src/com/TDC/skills/Skills.java
@@ -1,18 +1,30 @@
 package com.TDC.skills;
 
 public enum Skills {
-	HI(1, 1, 1, "hi");
+	LUCKY(true, 0.05, 0.05, 50, "Slightly improves schooling, mental health and financial situation"), 
+	INTELLIGENCE(true, 0.2, 0, 0, "Drastically improves schooling"), 
+	SOCIAL_SKILLS(true, 0, 0.2, 0, "Drastically improves mental health"), 
+	FINANCIAL_LITERACY(true, 0, 0, 200, "Drastically improves financial literacy"), 
+	GOOD_STUDY_HABBITS(true, 0.1, 0.1, 0, "Improves schooling and mental health"),
+	LIGHT_SLEEPER(true, 0.1, 0.1, 0, "Improves schooling and mental health"),
+	DISTRACTABLE(false, -0.1, -0.1, 0, "Reduces schooling and mental health"), 
+	INSOMNIAC(false, -0.1, -0.1, 0, "Reduces schooling and mental health"), 
+	POOR_HEALTH(false, -0.05, -0.05, -50, "Slightly reduces schooling, mental health and financial situation"), 
+	UNLUCKY(false, -0.05, -0.05, -50, "Slightly reduces schooling, mental health and financial situation");
+	
 	
 	private Skill skill;
+	private boolean positive;
 	
-	private Skills(double academic, double mentalHealth, double financials, String description) {
+	private Skills(boolean positive, double academic, double mentalHealth, double financials, String description) {
+		this.positive = positive;
 		this.skill = new Skill(academic, mentalHealth, financials, toTitle(this.toString()), description);
 	}
 	
 	private String toTitle(String s) {
 		String res = "";
 		
-		for (String w : s.split(" "))
+		for (String w : s.split("_"))
 			res += w.substring(0, 1).toUpperCase() + w.substring(1, w.length());
 		
 		return res;
@@ -20,5 +32,9 @@ public enum Skills {
 	
 	public Skill getSkill() {
 		return skill;
+	}
+	
+	public boolean isPositive() {
+		return positive;
 	}
 }
