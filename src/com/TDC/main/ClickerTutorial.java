8029fd54763553cdcace35e4d46be5dfad121ec2
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2022-02-19 20:15:38 -0700

    Month params

--- src/com/TDC/main/ClickerTutorial.java
+++ src/com/TDC/main/ClickerTutorial.java
@@ -1,12 +1,16 @@
+package com.TDC.main;
+
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class ClickerTutorial {
     public void getTutorial(){
         VBox beebleborx = new VBox();
-        beebleborx.getChildren().add(new Label = "When exam time comes, unscramble the anagrams in the time limit to solve the problem!");
+        beebleborx.getChildren().add(new Label("When exam time comes, unscramble the anagrams in the time limit to solve the problem!"));
         Scene scene = new Scene(beebleborx, 800, 600);
-        Window window = new Window();
+        Stage window = new Stage();
         window.setScene(scene);
         window.setTitle("How to Play");
         window.show();
